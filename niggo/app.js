758ddb3c07f93bbd59f5b37478e64a6895a90611
선우 <ksw-12345@naver.com>
2023-04-06 16:52:54 +0900

    230406
    array1

--- niggo/app.js
+++ niggo/app.js
@@ -1,2 +1,13 @@
-const anIFat = true;
-console.log(anIFat);
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
+
+const daysOfWeek = [mon , tue , wed , thu , fri , sat , sun];
+
+const nonsense = [1, 2, "hello", false, null, true, undefined, "nico"];
+
+console.log(daysOfWeek, nonsense);
